e6b7713a514c6b357fbeed52a33ffea8645655b4
alexaNovikova <alexadubinina@mail.ru>
2024-06-06 17:55:11 +0700

    Первая лекция, семинар

---
+++ src/main/java/ru/novikova/lesson1/BasicHero.java
@@ -0,0 +1,11 @@
+package ru.novikova.lesson1;
+
+public class BasicHero {
+    private int name;
+    private int ph;
+    private int maxHp;
+
+    public BasicHero(int name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/ru/novikova/lesson1/Ex002/Point2D.java
@@ -0,0 +1,6 @@
+package ru.novikova.lesson1.Ex002;
+
+public class Point2D  {
+    int x, y;
+    
+}
